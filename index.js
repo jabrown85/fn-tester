f3e314d1b3b57904359fdae6d32fac32cea05b0a
Jesse Brown <jabrown85@gmail.com>
2019-08-14 12:16:02 -0500

    Remove cloudevent parsing and riff message usage

--- index.js
+++ index.js
@@ -1,43 +1,9 @@
-const { Message } = require('@projectriff/message');
-const v03 = require('cloudevents-sdk/v03')
-
 userFn = async payload => {
   console.log('USERFN_PAYLOAD:', payload)
-  return {success: true};
-}
-
-middleware = async message => {
-  console.log('===New Message===');
-  console.log('PAYLOAD:', message.payload)
-  console.log('HEADERS:', message.headers)
-
-  const unmarshaller = new v03.HTTPUnmarshaller();
-  let result = null;
-
-  // if we detect either structured or binary cloudevent payload, parse with the cloudevent sdk
-  // this is triggered by knative cron/event source or direct with the right envelope
-  if (message.headers.getValue('content-type').indexOf('application/cloudevents+json') > -1 ||
-      message.headers.getValue('ce-specversion') != null) {
-    // it's a structured or binary cloudevent, let's parse it into a cloudevent
-    const protoHeaders = message.headers.toRiffHeaders()
-    // beat the riff headers into submission for cloudevents sdk
-    Object.keys(protoHeaders).map((key) => {protoHeaders[key] = message.headers.getValue(key)})
-    try {
-      const cloudevent = await unmarshaller.unmarshall(message.payload, protoHeaders)
-      console.log('CLOUDEVENT:', cloudevent)
-      result = await userFn(cloudevent.getData())
-    } catch (error) {
-      console.log('ERR:', error)
-    }
-  } else {
-    // this would be a direct HTTP interaction with the function
-    result = await userFn(message.payload)
+  if (payload){
+    return payload;
   }
-  console.log('RESULT:', result)
-  console.log('===End Message===');
-  return result;
+  console.log("NO RETURN")
 }
 
-module.exports = middleware;
-module.exports.$argumentType = 'message';
-Message.install();
+module.exports = userFn;
