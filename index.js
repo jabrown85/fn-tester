42d23d9cdeee831d441f771cfa4cb7436e3584c3
Jesse Brown <jabrown85@gmail.com>
2019-08-27 12:43:48 -0500

    another change

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 userFn = async payload => {
-  console.log('build me 123')
   console.log('USERFN_PAYLOAD:', payload)
   if (payload){
-    return payload;
+    return {whatever:true}
   }
   console.log("NO RETURN")
 }
